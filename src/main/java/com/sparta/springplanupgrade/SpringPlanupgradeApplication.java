f6d06e00a0a94fed13ebc0ed5006af0823b45e30
여준서 <duwnstj12@naver.com>
2024-08-27 20:20:03 +0900

    init

---
+++ src/main/java/com/sparta/springplanupgrade/SpringPlanupgradeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springplanupgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlanupgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlanupgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springplanupgrade/SpringPlanupgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springplanupgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlanupgradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
